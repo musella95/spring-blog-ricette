dfc374adc5c4bd55916d17ac99a599c7e522d1ff
musella95 <musellamatteoo@hotmail.com>
2024-01-22 11:04:12 +0100

    add get mapping

--- src/main/java/org/generation/springblogricette/controller/RecipeController.java
+++ src/main/java/org/generation/springblogricette/controller/RecipeController.java
@@ -3,12 +3,17 @@ package org.generation.springblogricette.controller;
 import org.generation.springblogricette.model.Recipe;
 import org.generation.springblogricette.repository.RecipeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/recipes")
@@ -19,5 +24,16 @@ public class RecipeController {
         List<Recipe> recipeList= recipeRepository.findAll();
         model.addAttribute("recipeList",recipeList);
         return "/recipes/index";
+    }
+    @GetMapping("/show/{id}")
+    public String show(@PathVariable Integer id, Model model) {
+        Optional<Recipe> result = recipeRepository.findById(id);
+        if (result.isPresent()) {
+            Recipe recipe = result.get();
+            model.addAttribute("recipe", recipe);
+            return "recipes/show";
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Recipe " + id + " not found");
+        }
     }
 }
